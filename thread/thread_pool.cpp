17732c853a17bc94dcb93c7bdd87eb024de610a2
piupiupiuboom <throughthearbor@163.com>
2023-05-09 21:26:14 +0800

    thread_pool debugging

--- thread/thread_pool.cpp
+++ thread/thread_pool.cpp
@@ -1,22 +1,46 @@
 #include "SPSCQueue.hpp"
 
+class TASK{
+public:
+    void dosome(void)
+    {
+        cout << "a task" << endl;
+    }
+};
 
-template <class T>
 class thread_pool{
 private:
-    SPSCQueue<T> tasks;
-    SPSCQueue<T> available;
-public:
-    explicit thread_pool(size_t totTreads)
-        :tasks(totTreads),available(totTreads){}
-    bool assign_task(void){
-        task=tasks.pop();
+    SPSCQueue<TASK> tasks;
+private:
+    static void* threadfunc(void* arg)
+    {
+        thread_pool* self = static_cast<thread_pool*> (arg);
+        while(1)
+        {
+            unique_ptr<TASK> task=std::move(self->tasks.pop());
+            task->dosome();
+        }
+        return 0;
     }
-    bool work(void);
+public:
+    explicit thread_pool(size_t tottasks,size_t totTreads)
+        :tasks(tottasks){
+            for (int i=0;i<totTreads;i++)
+            {
+                pthread_t thread;
+                pthread_create(&thread,NULL,&threadfunc,this);
+            }
+        }
+    bool add_task(unique_ptr<TASK> task){
+        return tasks.Push(std::move(task));
+    }
 };
 
 int main(void)
 {
+    thread_pool pool(7,4);
+    for (int i=0;i<9;i++)
+        pool.add_task(std::make_unique<TASK>());
     return 0;
 }
 
